d5d129cdf486b6f164e1eefdedc009c9f22a7bb4
jhonys-santos <jhonys@seubone.com>
2026-07-03 13:58:43 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Cole aqui a URL do seu App da Web do Apps Script (termina em /exec)
 // É o único lugar que precisa editar depois de implantar o backend.
-const APP_URL = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
+const APP_URL = "https://script.google.com/macros/s/AKfycby4xXDFZ-bNbdoMUQBbf1C-eBeYBnKAxUbJlS_Izg61OSc7EKp0fQZ64i2bfgECMN58/exec";
