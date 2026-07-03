1dae15591593f00105be65f0b1df23c27323e247
jhonys-santos <jhonys@seubone.com>
2026-07-03 15:27:14 -0300

    Update APP_URL to the live Apps Script URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Cole aqui a URL do seu App da Web do Apps Script (termina em /exec)
 // É o único lugar que precisa editar depois de implantar o backend.
-const APP_URL = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
+const APP_URL = "const APP_URL = "https://script.google.com/macros/s/AKfycbxe3YpD8JzvFxgb_T0ndkT2LzlphjS27qGqJvnAs-qgkjm67PApOT6q7GUVhkQpAGRW/exec";";
