519e713801b0b359bcdfe43562b78c19fc0a986b
jhonys-santos <jhonys@seubone.com>
2026-07-03 14:11:30 -0300

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Cole aqui a URL do seu App da Web do Apps Script (termina em /exec)
-// É o único lugar que precisa editar depois de implantar o backend.
-const APP_URL = "https://script.google.com/macros/s/AKfycby4xXDFZ-bNbdoMUQBbf1C-eBeYBnKAxUbJlS_Izg61OSc7EKp0fQZ64i2bfgECMN58/exec";
