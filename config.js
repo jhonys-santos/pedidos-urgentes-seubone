b6074c2b3fc1a3abad25ee777a5be191af5f02f9
jhonys-santos <jhonys@seubone.com>
2026-07-03 15:58:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// Cole aqui a URL do seu App da Web do Apps Script (termina em /exec)
-// É o único lugar que precisa editar depois de implantar o backend.
-const APP_URL = "const APP_URL = "https://script.google.com/macros/s/AKfycbxe3YpD8JzvFxgb_T0ndkT2LzlphjS27qGqJvnAs-qgkjm67PApOT6q7GUVhkQpAGRW/exec";";
+const APP_URL = "https://script.google.com/macros/s/AKfycbxe3YpD8JzvFxgb_T0ndkT2LzlphjS27qGqJvnAs-qgkjm67PApOT6q7GUVhkQpAGRW/exec";
